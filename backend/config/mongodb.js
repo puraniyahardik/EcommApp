3b3c79c078f9d3f329ff147872619ba6601f0270
puraniya hardik <puraniyahardik2829@gmail.com>
2025-01-24 18:50:33 +0530

    second commit

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,27 +1,29 @@
 import mongoose from 'mongoose';
 
 
-const connectDB= async ()=>{
-  await  mongoose
-  .connect(process.env.MONGODB_URL)
-  .then((e)=> console.log('MongoDb connected.....')) 
-  .catch((e)=> console.log(e))  
-}
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URL);
+// const connectDB= async ()=>{
+//   await  mongoose
+//   .connect(process.env.MONGODB_URL)
+//   .then((e)=> console.log('MongoDb connected.....')) 
+//   .catch((e)=> console.log(e))  
+// }
 
-//         mongoose.connection.on('connected', () => {
-//             console.log('DB Connected');
-//         });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('connected');
+        
 
-//         mongoose.connection.on('error', (err) => {
-//             console.error('MongoDB connection error:', err.message);
-//         });
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB:', error.message);
-//         process.exit(1); // Exit process with failure
-//     }
-// };
+        mongoose.connection.on('connected', () => {
+            console.log('DB Connected');
+        });
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err.message);
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1); // Exit process with failure
+    }
+};
 
 export default connectDB;
